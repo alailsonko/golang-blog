090e4f866fb0b1158a958428fc0321b11331b585
alailsonko <aalailson3@gmail.com>
2020-12-03 22:19:07 -0300

    feat: implements file upload

--- src/login.gtpl
+++ src/login.gtpl
@@ -6,6 +6,7 @@
 <form action="/login" method="POST">
 Username:<input type="text" name="username">
 Password:<input type="password" name="password">
+<input type="hidden" name="token" value="{{.}}">
 <input type="submit" value="Login">
 </form>
 </body>
--- src/main.go
+++ src/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -20,24 +24,73 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	
+
 	fmt.Fprintf(w, "hello world")
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
+func locateFiles(path string) string {
 	cwd, _ := os.Getwd()
+	location := filepath.Join(cwd, path)
+	return location
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) // get request method
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles(filepath.Join(cwd, "./src/login.gtpl"))
-		err := t.Execute(w, nil)
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+		fmt.Println(token)
+		filePath := "./src/login.gtpl"
+		t, _ := template.ParseFiles(locateFiles(filePath))
+		err := t.Execute(w, token)
 		if err != nil {
 			fmt.Println("err:", err)
 		}
 	} else {
 		r.ParseForm()
-		// logic part of log in
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
+		token := r.Form.Get("token")
+		fmt.Println("my token: ", token)
+		if token != "" {
+			// check token validity
+		} else {
+			// give error if no token
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles(locateFiles("./src/upload.gtpl"))
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile(locateFiles("./src/files/")+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
 	}
 }
 
@@ -45,6 +98,7 @@ func main() {
 
 	http.HandleFunc("/", sayhelloName)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
 
 	err := http.ListenAndServe(getPort(), nil)
 	if err != nil {
