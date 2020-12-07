3598e0db55bd41eb0c71bcb41c154c7a4671e651
alailsonko <aalailson3@gmail.com>
2020-12-06 22:18:21 -0300

    fix: fix deploy

--- src/main.go
+++ src/main.go
@@ -3,26 +3,27 @@ package main
 import (
 	"fmt"
 	_ "golang-CRUD/src/routers"
-	"log"
 	"os"
 	"strconv"
 
 	beego "github.com/astaxie/beego/server/web"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load("../.env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-	portEnv := os.Getenv("PORT")
-	fmt.Println("env", portEnv)
-	port, err := strconv.Atoi(portEnv)
-	if err != nil {
-		log.Fatal(err)
-	}
-	beego.BConfig.Listen.HTTPPort = port
+	beego.BConfig.Listen.HTTPPort = getPort()
 	beego.Run()
+}
+
+func getPort() int {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		portEnv, _ := strconv.Atoi("9090")
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+		return portEnv
+	}
+	portEnv, _ := strconv.Atoi(port)
+
+	return portEnv
 }
